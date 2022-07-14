bd843047197492cd3f6904afd8f34f6ab1d74c2d
joao <joaoeifler2004@gmail.com>
2022-07-13 21:25:23 -0400

    finished the basic grid

---
+++ script.js
@@ -0,0 +1,21 @@
+let board = document.querySelector("#board");
+const boardSize = 500;
+let resolution = 16;
+let lineHeight = boardSize/resolution; 
+
+for (let i = 0; i < resolution; i++) {
+    let line = document.createElement("div");
+    line.className = "line";
+    line.style.height = `${lineHeight}px`;
+    
+    for (let j = 0; j < resolution; j++) {
+        let square = document.createElement("div");
+        square.className = "square";
+        square.style.height = `${lineHeight}px`;
+        square.style.width = `${lineHeight}px`;
+
+        line.appendChild(square);
+    }
+
+    board.appendChild(line);
+}
